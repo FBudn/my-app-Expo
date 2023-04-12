78f4779501ee5b6b247e41e05411d6a1c7f9ab37
FBudn <111660524+FBudn@users.noreply.github.com>
2023-04-12 10:13:15 +0200

    gitAction test

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MY TEXT</Text>
+      <Text>MY TEXT test</Text>
     </View>
   );
 }
